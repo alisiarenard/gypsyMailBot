11f9bcaccfa9ebb6a57c551f41fa9b7e322626f8
alisia.mikhaylova <alisia.mikhaylova@nordigy.ru>
2023-08-08 16:00:46 +0100

    fixed run npm

--- src/connections/db.connection.ts
+++ src/connections/db.connection.ts
@@ -1,7 +1,9 @@
 import { Sequelize } from'sequelize';
+import dotenv from "dotenv";
 
 // @ts-ignore
 // @ts-ignore
+dotenv.config();
 export const db = new Sequelize(
     process.env.POSTGRES_DATABASE as string,
     process.env.POSTGRES_USERNAME as string,
