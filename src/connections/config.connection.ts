6ce2079e3533fca7ccbf009fd8c282497db296d4
alisia.mikhaylova <alisia.mikhaylova@nordigy.ru>
2023-08-10 16:49:33 +0100

    add config.ts

---
+++ src/connections/config.connection.ts
@@ -0,0 +1,9 @@
+export const config = {
+    botToken: "6136046432:AAF7Pd_zN7WQdR2FEWUkgHG-_8Eo1Cz-x9E",
+    database: "postgres",
+    username: "postgres",
+    password: "postgres",
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: "postgres"
+}
--- src/connections/db.connection.ts
+++ src/connections/db.connection.ts
@@ -1,14 +1,5 @@
 import {Dialect, Sequelize} from 'sequelize';
-
-export const config = {
-    botToken: "6136046432:AAF7Pd_zN7WQdR2FEWUkgHG-_8Eo1Cz-x9E",
-    database: "postgres",
-    username: "postgres",
-    password: "postgres",
-    host: "127.0.0.1",
-    port: 5432,
-    dialect: "postgres"
-}
+import {config} from "./config.connection.js";
 
 export const db = new Sequelize(config.database, config.username, config.password,
     {
